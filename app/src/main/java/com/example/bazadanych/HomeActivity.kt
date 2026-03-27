7b1d2950c6bd307c33b92022460390188f1ad644
Krzysztof <krzysztofkaz3@onet.eu>
2026-03-27 09:51:28 +0100

    test

--- app/src/main/java/com/example/bazadanych/HomeActivity.kt
+++ app/src/main/java/com/example/bazadanych/HomeActivity.kt
@@ -8,7 +8,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.content.edit
-
+//test
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
